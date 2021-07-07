8c6abbaa0cabba747027affe35fe574864b9f93d
Your Name <xujuntao1616@outlook.com>
2021-07-07 15:19:37 +0800

    finish pic_rotate.py

--- utils/pic_rotate.py
+++ utils/pic_rotate.py
@@ -2,10 +2,9 @@ import cv2
 import math
 
 
-def pic_rotate(img, bboxes, angle):
+def rotate(img, bboxes, angle):
     h, w = img.shape[:2]
-    xmid = w / 2
-    ymid = h / 2
+    xmid, ymid = w / 2, h / 2
 
     rotation_img = cv2.getRotationMatrix2D((xmid, ymid), angle, 1)
 
@@ -26,49 +25,43 @@ def pic_rotate(img, bboxes, angle):
     radian = -1 * math.pi * (angle / 180)
     rotated_bboxes = []
     for bbox in bboxes:
-        xmin, ymin, xmax, ymax = bbox
-
-        xmin -= xmid
-        ymin -= ymid
-        xmax -= xmid
-        xmax -= ymid
+        xmin_to_origin = bbox[0] - xmid
+        ymin_to_origin = bbox[1] - ymid
+        xmax_to_origin = bbox[2] - xmid
+        ymax_to_origin = bbox[3] - ymid
 
         '''
         Affine transformation, anticlockwise with positive rotate_degrees
-        x' = cos(degree) * x - sin(degree) * y
-        y' = sin(degree) * x + cos(degree) * y
+        x' = cos(radian) * x - sin(radian) * y
+        y' = sin(radian) * x + cos(radian) * y
         '''
 
         sin = math.sin(radian)
         cos = math.cos(radian)
+        r_xmin = cos * xmin_to_origin - sin * ymin_to_origin + (bound_w / 2)
+        r_ymin = sin * xmin_to_origin + cos * ymin_to_origin + (bound_h / 2)
+        r_xmax = cos * xmax_to_origin - sin * ymax_to_origin + (bound_w / 2)
+        r_ymax = sin * xmax_to_origin + cos * ymax_to_origin + (bound_h / 2)
 
-        rotated_xmin = cos * xmin - sin * ymin + (bound_w / 2)
-        rotated_ymin = sin * xmin + cos * ymin + (bound_h / 2)
-        rotated_xmax = cos * xmax - sin * ymax + (bound_w / 2)
-        rotated_ymax = sin * xmax + cos * ymax + (bound_h / 2)
-
-        rotated_bboxes.append([int(rotated_xmin), int(rotated_ymin), int(rotated_xmax), int(rotated_ymax)])
+        rotated_bboxes.append([int(r_xmin), int(r_ymin), int(r_xmax), int(r_ymax)])
 
     return rotated_img, rotated_bboxes
 
 
-if __name__ == '__main__':
-    # test
-    angle = 30
-    bboxes = [[1201, 621, 1233, 691]]
-    # bboxes = [[1201, 621, 1233, 691], [1099, 455, 1122, 540], [1084, 789, 1071, 762]]
-    img = cv2.imread("D:/2021hwsz/Images/2020-01-11_21_36_02_642.jpg")
+def draw(img, bboxes):
     for bbox in bboxes:
-        print(bbox)
         cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2)
     cv2.imshow("img", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
+if __name__ == '__main__':
+    angle = 30
+    bboxes = [[1201, 621, 1233, 691], [1099, 455, 1122, 540], [1084, 789, 1071, 762]]
     img = cv2.imread("D:/2021hwsz/Images/2020-01-11_21_36_02_642.jpg")
-    rotated_img, rotated_bboxes = pic_rotate(img, bboxes, angle)
-    for rotated_bbox in rotated_bboxes:
-        cv2.rectangle(rotated_img, (rotated_bbox[0], rotated_bbox[1]), (rotated_bbox[2], rotated_bbox[3]), (0, 0, 255), 2)
-    cv2.imshow("rotated_img", rotated_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    draw(img, bboxes)
+
+    img = cv2.imread("D:/2021hwsz/Images/2020-01-11_21_36_02_642.jpg")
+    rotated_img, rotated_bboxes = rotate(img, bboxes, angle)
+    draw(rotated_img, rotated_bboxes)
